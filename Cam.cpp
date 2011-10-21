4d5490f18a184f07f9d6c6299c5248af96ee3fe1
Kostas Tamateas <nosebleedKT@gmail.com>
2011-10-21 23:08:14 +0300

    Update Cam.cpp

--- Cam.cpp
+++ Cam.cpp
@@ -1,3 +1,11 @@
+/*
+    Copyright (C)2011 Kostas Tamateas <nosebleedKT@gmail.com>
+    This program is distributed under the terms of the GNU
+    General Public License, version 2. You may use, modify,
+    and redistribute it under the terms of this license.
+    A copy should be included with this source.
+*/
+
 #include "Cam.h"
 
 Fat16 pictureFile;
